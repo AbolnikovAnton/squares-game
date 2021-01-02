ab4da96944d4f883abdfa9f13455d69315326c87
unknown <bakunin.abolnikov@gmail.com>
2021-01-02 12:00:46 +0200

    created Square by div and css

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent implements OnInit {
+  color;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
