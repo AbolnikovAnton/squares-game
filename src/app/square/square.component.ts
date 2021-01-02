b93c303cfbd43b38a2a1ef4c9236fa2d5a147da6
unknown <bakunin.abolnikov@gmail.com>
2021-01-02 12:04:13 +0200

    create method randomColor

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -8,6 +8,14 @@ import {Component, OnInit} from '@angular/core';
 export class SquareComponent implements OnInit {
   color;
 
+  randomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.color = color;
+  }
   constructor() {
   }
 
